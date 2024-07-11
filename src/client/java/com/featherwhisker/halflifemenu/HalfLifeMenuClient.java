a7cf5b5a4d8c353f20bb88179a1d72e877cfb5f7
Scillman <Scillman@users.noreply.github.com>
2024-07-11 17:37:45 +0200

    Initial commit

---
+++ src/client/java/com/featherwhisker/halflifemenu/HalfLifeMenuClient.java
@@ -0,0 +1,11 @@
+package com.featherwhisker.halflifemenu;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HalfLifeMenuClient implements ClientModInitializer
+{
+    @Override
+    public void onInitializeClient()
+    {
+    }
+}
---
+++ src/main/java/com/featherwhisker/halflifemenu/HalfLifeMenu.java
@@ -0,0 +1,19 @@
+package com.featherwhisker.halflifemenu;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HalfLifeMenu implements ModInitializer
+{
+    public static final String MOD_ID = "halflifemenu";
+    public static final String MOD_NAME = "Half-Life Menu";
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
+
+    @Override
+    public void onInitialize()
+    {
+    }
+}
